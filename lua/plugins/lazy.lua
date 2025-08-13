1e1332898f9be5d9fcd407348aacdd75acaf8f01
Sean Price <sean@complyauto.com>
2025-08-13 11:47:59 -0400

    Tree follow focused file

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -29,7 +29,11 @@ require("lazy").setup({
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup {
+        update_focused_file = {
+          enable = true,
+        }
+      }
     end,
   },
 
