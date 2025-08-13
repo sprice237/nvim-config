2af90ad54d992cf2598cacaea97b098f20fd2437
Sean Price <sean@complyauto.com>
2025-08-13 12:32:38 -0400

    Add space space to find file

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -1,5 +1,6 @@
 -- telescope
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<cr>")
+vim.keymap.set("n", "<leader> ", ":Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>fg", ":Telescope git_files<cr>")
 vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<cr>")
 vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>")
